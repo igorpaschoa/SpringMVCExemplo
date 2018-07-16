774c8d35950a7a178e7d587435982043c63da239
Igor Paschoa <igor_paschoa@hotmail.com>
2018-07-16 19:40:38 -0300

    Correção bugs configuração spring

--- src/br/com/treinaweb/springmvc/controllers/HelloController.java
+++ src/br/com/treinaweb/springmvc/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package br.com.treinaweb.springmvc.contrtollers;
+package br.com.treinaweb.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
